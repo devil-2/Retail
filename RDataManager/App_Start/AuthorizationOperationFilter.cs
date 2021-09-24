9aea1517b4fba929c6d444fde991fa6000aa0a67
Mishra Vikas <vikas.mishra@consultant.volvo.com>
2021-09-24 16:42:49 +0530

    SwaggerCongig

---
+++ RDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,27 @@
+﻿using WebActivatorEx;
+using RDataManager;
+using Swashbuckle.Swagger;
+using System.Web.Http.Description;
+using System.Collections.Generic;
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace RDataManager
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null) operation.parameters = new List<Parameter>();
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access_token",
+                required=false,
+                type="string"
+            });
+
+        }
+    }
+}

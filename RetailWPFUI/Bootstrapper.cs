160a500016b30056f67cf820f0a5c19df9f2cbae
Mishra Vikas <Vikas.mishra@consultant.volvo.com>
2021-09-27 15:53:07 +0530

    Added WPFUI and Caliburn Micro

---
+++ RetailWPFUI/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using Caliburn.Micro;
+using RetailWPFUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace RetailWPFUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            RegisterCommonTypes("ViewModel");
+        }
+
+        private void RegisterCommonTypes(string type)
+        {
+            GetType().Assembly.GetTypes()
+                .Where(t => t.IsClass)
+                .Where(t => t.Name.EndsWith(type))
+                .ToList()
+                .ForEach(view => _container.RegisterPerRequest(view, view.ToString(), view));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+
+}
